e9711c136b48e93aaebb76627500d89be998753d
gkavetha2024-gif <gkavetha2024@gmail.com>
2026-07-16 15:24:56 -0600

    Delete Apache_Server directory

--- Apache_Server/apache_web.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo root
-sudo hostnamectl set-hostname AWSAPACHESERVER.MYPC.COM
-echo "`hostname -I | awk '{print $1}'` `hostname`" >> /etc/hosts
-sudo apt update
-sudo apt install apache2 -y
-sudo apt install unzip
-timedatectl set-timezone "Asia/Kolkata"
